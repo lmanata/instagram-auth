9e3df09f52ad6ed5e1874677ce7c668ce707e62d
luis manata <luism1425@gmail.com>
2018-03-01 22:30:31 +0000

    move exception to util file

--- util.php
+++ util.php
@@ -30,3 +30,19 @@ function simpleJsonPost ( $url, $data ) {
 
   return $result;
 }
+
+class DetailedException extends Exception {
+  private $result;
+  private $request;
+
+  public function __construct ( $message, $request = [], $result = [] ) {
+    $this->request = $request;
+    $this->result = $result;
+
+    parent::__construct( $message );
+  }
+
+  public function getVar ( $key ) {
+    return $this->{$key};
+  }
+}
